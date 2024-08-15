e1753ba9acb633b287781d84489e16a3239d1f1f
Alexander Engman <alexanderengman@ASCILION010.localdomain>
2024-08-15 15:10:59 +0200

    Test

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,39 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-    $to = "alexander@engman.nu";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+$to = "alexander@engman.nu";
+$from = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+$name = htmlspecialchars($_REQUEST['name']);
+$subject = htmlspecialchars($_REQUEST['subject']);
+$cmessage = htmlspecialchars($_REQUEST['message']);
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$headers = "From: " . $from . "\r\n";
+$headers .= "Reply-To: ". $from . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+$logo = 'img/engman.png'; // Use absolute URL
+$link = 'https://www.alexanderengman.com/'; // Use your actual website URL
 
-    $logo = 'img/Engman.png';
-    $link = '#';
+$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+$body .= "<table style='width: 100%;'>";
+$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+$body .= "</td></tr></thead><tbody><tr>";
+$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+$body .= "</tr>";
+$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+$body .= "<tr><td></td></tr>";
+$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+$body .= "</tbody></table>";
+$body .= "</body></html>";
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+$send = mail($to, $subject, $body, $headers);
 
-    $send = mail($to, $subject, $body, $headers);
-
-?>
+if ($send) {
+    echo json_encode(['success' => true, 'message' => 'Email sent successfully']);
+} else {
+    echo json_encode(['success' => false, 'message' => 'Failed to send email']);
+}
